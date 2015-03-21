507ed40754e04300b49067e3242e42e4bc82f472
azhel12 <azhel12reg@gmail.com>
2015-03-22 00:22:41 +0300

    Add a new class Game. It must orginize game between 2 players

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+
+    String _player;
+    DataBaseWorker _dbworker;
+    public Game(String player, DataBaseWorker dbworker){
+        _player = player;
+        _dbworker = dbworker;
+    }
+
+    public void Game()
+    {
+
+    }
+}
