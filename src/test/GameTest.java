97925f9b7abd206d9614d8d6d2921a1715e918c2
azhel12 <azhel12reg@gmail.com>
2015-03-22 12:58:25 +0300

    Tests

---
+++ src/test/GameTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+import src.Game;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void testOrganizeGame() throws Exception {
+        //Game game= new Game("user","user");
+        //game.OrganizeGame();
+    }
+}
