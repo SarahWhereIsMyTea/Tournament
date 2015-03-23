d1b53dca17f7b6c168cc25908f52b91eef2e3b6b
azhel12 <azhel12reg@gmail.com>
2015-03-23 22:27:03 +0300

    Add 1 argument - file name. When user load new source code, system can copy it anywhere and our program get this path.

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if(args.length != 2)
+        if(args.length != 2)    // принимаем 3 аргумента - имя пользователя, игру и имя файла кода
             return;
 
-        Tournament tournament = new Tournament(args[0], args[1]);
+        Tournament tournament = new Tournament(args[0], args[1], args[2]);
+
+        int gameResult = tournament.Game();
 
         return;
     }
