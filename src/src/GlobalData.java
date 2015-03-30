04d47506590d596bf510a0a6986d4442be604846
aaa <aaa@gmail.com>
2015-03-30 21:15:50 +0300

    For more fast and good code write this class with pattern "Singleton".

---
+++ src/src/GlobalData.java
@@ -0,0 +1,73 @@
+package src;
+
+import org.ini4j.Wini;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class GlobalData {
+
+    public static volatile GlobalData instance;
+
+    public static GlobalData getInstance(){
+        if(instance == null)
+            synchronized (GlobalData.class){
+                if(instance == null)
+                    instance = new GlobalData();
+            }
+        return instance;
+    }
+
+    String Root;
+    String SDKPath;
+    String UserPath;
+    String DBServer;
+    String DBTable;
+    String DBLogin;
+    String DBPassword;
+    String Judge;
+    String Delimer;
+
+    public GlobalData(){
+        Root = GetParam("Root");
+        SDKPath = GetParam("SDKPath").replaceAll("%Root%", Root);
+        UserPath = GetParam("UserPath").replaceAll("%Root%", Root);
+        DBServer = GetParam("DBServer");
+        DBTable = GetParam("DBTable");
+        DBLogin = GetParam("DBLogin");
+        DBPassword = GetParam("DBPassword");
+        Judge = GetParam("Judge");
+        Delimer = GetDelimer();
+    }
+
+    private String GetParam(String keyName) {
+        Wini ini = null;
+        try {
+            ini = new Wini(new File("tournamentset.ini"));
+        } catch (IOException e) {
+            return "Error";
+        }
+
+        return ini.get("General", keyName);
+    }
+
+    private String GetDelimer(){
+        if(System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) //если windows
+            return "\\";
+
+        if(System.getProperty("os.name").toLowerCase().indexOf("nux") >= 0 ||
+                System.getProperty("os.name").toLowerCase().indexOf("nix") >= 0) //если linux / unix
+            return "/";
+
+        return "/";
+    }
+
+    public static String GetLang(Path path){
+        return path.getName(path.getNameCount() - 2).toString();
+    }
+
+    public static String GetUsername(Path path) {
+        return path.getName(path.getNameCount() - 3).toString();
+    }
+}
