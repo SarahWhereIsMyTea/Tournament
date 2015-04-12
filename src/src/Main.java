4d78da3f59dc7b1fd2cc8f366ac8d627ac7de9c8
azhel <azhel12reg@gmail.com>
2015-04-12 16:02:22 -0700

    Add try...catch for IOException. There are many places, where this exception may appear.

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,21 @@
 package src;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        if(args.length != 2)    // принимаем 3 аргумента - имя пользователя, игру и имя файла кода
+        if(args.length != 3)    // принимаем 3 аргумента - имя пользователя, игру и имя файла кода
             return;
 
         Tournament tournament = new Tournament(args[0], args[1], args[2]);
 
-        int gameResult = tournament.Game();
+        try {
+            int gameResult = tournament.Game();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return;
     }
